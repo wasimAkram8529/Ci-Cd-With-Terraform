8d67606b5dcbf7438b54ce4ab38aaafb9a8e5ebd
wasimAkram8529 <codelikearockstar@gamil.com>
2025-12-26 06:06:11 +0000

    first commit

---
+++ terraform/output.tf
@@ -0,0 +1,3 @@
+output "website_bucket_name" {
+  value = aws_s3_bucket.remote_backend.bucket
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type = string
+  default = "my-static-site-using-terraform"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+}
