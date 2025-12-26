6dbee0177f6b081f343c82e97c67f492d5f87373
wasimAkram8529 <codelikearockstar@gamil.com>
2025-12-26 06:57:57 +0000

    S3 Bucket Policy error resolved

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,12 +40,18 @@ resource "aws_s3_bucket_policy" "website_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
+      "Sid": "Statement1",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ]
       Resource  = "${aws_s3_bucket.website.arn}/*"
     }]
   })
+  depends_on = [
+    aws_s3_bucket_public_access_block.website_public_access
+  ]
 }
 
 
